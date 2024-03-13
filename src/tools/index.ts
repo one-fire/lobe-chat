4ac08079252c8da514a25a2a14503ba821ece241
yi-ge <a@wyr.me>
2024-03-13 14:17:08 +0800

    ✨ feat: 禁用DALL E

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,11 +1,12 @@
 import { LobeBuiltinTool } from '@/types/tool';
 
-import { DalleManifest } from './dalle';
+// 禁止DALL·E
+// import { DalleManifest } from './dalle';
 
 export const builtinTools: LobeBuiltinTool[] = [
-  {
-    identifier: DalleManifest.identifier,
-    manifest: DalleManifest,
-    type: 'builtin',
-  },
+  // {
+  //   identifier: DalleManifest.identifier,
+  //   manifest: DalleManifest,
+  //   type: 'builtin',
+  // },
 ];
